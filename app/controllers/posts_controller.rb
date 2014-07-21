ff0872b25606514b7e15ddc82f6c5e55267353f9
lana chernenko <chernenko.lana@gmail.com>
2014-07-21 12:13:55 -0700

    commit changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all
+  end
+  def show
+    @post = Post.find{params[:id]}
+    render :show
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
   end
 end
